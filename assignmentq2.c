1e9940625ec34f20632b7087f7aa6b7da6dc28a0
kuhupatel15 <101010210+kuhupatel15@users.noreply.github.com>
2022-03-05 22:03:33 +0530

    NPTL

---
+++ assignmentq2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a , b ,c ;
+    a = 1;
+    scanf("%d",&a);
+
+    b = 2;
+    scanf("%d",&b);
+    c = 3 ;
+    scanf("%d",&c);
+    if (c>a+b)
+        printf("1");
+    else
+        printf("0");
+    return 0 ;
+}

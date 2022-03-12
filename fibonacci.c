71b43df5803c32657f762c834613fc983c3cf3ad
kuhupatel15 <101010210+kuhupatel15@users.noreply.github.com>
2022-03-12 21:38:00 +0530

    Fibonacci series

--- fibonacci.c
+++ fibonacci.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
     int num,a, b,i ,sum=0;
-    printf("Enter number of terms of fibonassi series \n");
+    printf("Enter number of terms of fibonacci series \n");
     scanf("%d",&num);
-    printf("Fibonassi series : \n");
+    printf("Fibonacci series : \n");
     i=0;
     b=1  ;
     printf("0  ");
